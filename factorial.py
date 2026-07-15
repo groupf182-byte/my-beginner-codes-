07a7f75752f8ad319b3686bfd85a55f1c2ba518f
groupf182-byte <groupf182@gmail.com>
2026-07-15 17:51:51 +0530

    Fix factorial calculation and input prompt

--- factorial.py
+++ factorial.py
@@ -1,9 +1,8 @@
 import pandas as pd
 x= int (input("entre the no for factorial "))
-q=x*2
 factorial = 1
 z=1
-while z<=q :
+while z<=x :
     factorial=factorial*z
     z=z+1
     print (f"factorial of {x} is {factorial}")
