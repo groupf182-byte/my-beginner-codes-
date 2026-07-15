8962e2f2fef8853f081388e141a2ac49b8090be8
groupf182-byte <groupf182@gmail.com>
2026-07-15 17:50:39 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,10 @@
+import pandas as pd
+x= int (input("entre the no for factorial "))
+q=x*2
+factorial = 1
+z=1
+while z<=q :
+    factorial=factorial*z
+    z=z+1
+    print (f"factorial of {x} is {factorial}")
+    
