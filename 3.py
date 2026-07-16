3e69b82b4937e105a758c4e2be897ef6dfb74d6d
groupf182-byte <groupf182@gmail.com>
2026-07-16 18:28:34 +0530

    Is the number is positive or negative .

---
+++ 3.py
@@ -0,0 +1,10 @@
+# Is the number is positive or negative.
+a=int (input ( "entre the number to check whether is positive or negative.____"))
+if a>0:
+    print("given nunmber is positive .")
+elif a==0:
+    print ('''
+given number is neither positive nor negative.
+given number is zero (0).''')
+else:
+    print ("given number is negative")
