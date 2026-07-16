0dce5dee7a990f9fb5bb54e901ff30ec3dd86641
groupf182-byte <groupf182@gmail.com>
2026-07-16 18:30:01 +0530

    Maximum of given three numbers .

---
+++ 4.py
@@ -0,0 +1,11 @@
+#Maximum of given three numbers.
+print ("maximun of given numbers")
+a=int(input("entre the first number__"))
+b=int(input("entre the Second number__"))
+c=int(input("entre the Third number__"))
+if a>b>c:
+    print(a,"is the maximum number")
+elif c>b>a:
+    print(c,"is the maximum number ")
+else :
+    print(b,"is the maximum number ")
