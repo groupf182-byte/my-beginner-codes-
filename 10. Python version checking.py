77770565a972e90389994fa37c106bf2eccebec9
groupf182-byte <groupf182@gmail.com>
2026-07-20 18:24:28 +0530

    Add files via upload

---
+++ 10. Python version checking.py
@@ -0,0 +1,8 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/mohit/OneDrive/Documents/python/Python version checking.py =
+python version
+3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)]
+version information
+sys.version_info(major=3, minor=14, micro=6, releaselevel='final', serial=0)
